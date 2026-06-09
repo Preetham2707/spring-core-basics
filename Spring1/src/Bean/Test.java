a6a133812b2b6742af66862c0978d706d033f9a3
Preetham Reddy Kallem <kallempreetham99@gmail.com>
2026-06-09 19:36:41 +0530

    Added Constructor Injection example

--- Spring1/src/Bean/Test.java
+++ Spring1/src/Bean/Test.java
@@ -2,16 +2,19 @@ package Bean;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 
 public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-//		Resource rc=new ClassPathResource("SpringConfig.xml");
+		Resource rc=new ClassPathResource("SpringConfig.xml");
 //		BeanFactory bn=new XmlBeanFactory(rc);
 		ApplicationContext bn=new ClassPathXmlApplicationContext("SpringConfig.xml");
 		Student s=(Student)bn.getBean("obj");
 //		System.out.println(s.getId()+" "+s.getName()+" "+s.getMarks());
 		System.out.println(s);
+		System.out.println("Hello World");
 	}
 }
